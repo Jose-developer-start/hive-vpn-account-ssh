0a54fbb57705eebb15619a6dff7746b71cfc80bd
jose dev <josededanes99@gmail.com>
2021-12-05 15:41:24 -0600

    Update view pago

---
+++ config/global.php
@@ -0,0 +1,13 @@
+<?php
+    //define('IP', '143.198.170.229');
+    define('IP_SSH', "192.168.43.89");
+    define('PASSWORD_SSH', "jose002");
+    define('PORT_SSH', "22");
+    define('USER_SSH',"root");
+    define('CLIENTID','ATYhHQHQmLSuWb91fq6uAIBQXW3vmnAPbKJvVkdz9o85qsoI4MB0sG9QOk6IWPBbSj3BkrWrp0hFdsZ4');
+    define('SECRET', 'EC3yb6dQAkQ-k-k4AsMPM6-HtMuvaxJc-a8WTbU5Jps2Xv9cxGZKGx3nBAIpEZ1d05s2LivHHNnPIBG3');
+    
+
+
+
+?>
--- premium/pago.php
+++ premium/pago.php
@@ -1,6 +1,5 @@
 <?php
     session_start();
-    session_start();
     if(!isset($_SESSION['auth'])){
         header('Location: ../index.php');
     }
@@ -32,29 +31,10 @@
         }
     }
 </style>
-<!--------------------------------------------AGREGANDO CODIGO PARA PAYPAL------------------------------------------->
-
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<div class="my-5">
-    <div class="row">
-        <div class="col-md-5 mx-auto">
-            <div class="card">
-                <div class="card-body">
-                    <div class="jumbotron text-center">
-                        <h1 class="display-4">Paso final</h1>
-                        <hr class="my-4">
-                        <p class="lead">Estas a punto de pagar con PayPal la cantidad de:</p>
-                        <h4>$<?php echo $total ?></h4>
-                        <div id="paypal-button-container"></div>
-                        <p>No hay reembolso de dinero!</p><br>
-                        <strong>@hive-vpn.tk</strong>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<!--AGREGANDO CODIGO PARA PAYPAL------------------------------------------->
+<?php
+    include_once "./view_pago.php";
+?>
 
 <script>
     paypal.Button.render({
